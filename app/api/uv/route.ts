18632ae42dafe82bc00d977ff52d732a6aded49e
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2024-10-07 21:44:01 +0200

    fix api-uv

--- app/api/uv/route.ts
+++ app/api/uv/route.ts
@@ -12,10 +12,11 @@ export async function GET(request: Request) {
       { status: 400 }
     );
   }
+
   const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=uv_index_max,uv_index_clear_sky_max&timezone=auto&forecast_days=1`;
+
   try {
     const response = await axios.get(url);
-
     const { daily } = response.data;
 
     if (!daily || !daily.uv_index_max || daily.uv_index_max.length === 0) {
@@ -25,9 +26,18 @@ export async function GET(request: Request) {
       );
     }
     return NextResponse.json({ uvIndex: daily.uv_index_max[0] });
-  } catch (error: any) {
+
+  } catch (error: unknown) { // Use `unknown` instead of `any`
+    // Type guard to handle the error safely
+    if (axios.isAxiosError(error)) {
+      return NextResponse.json(
+        { error: `Error fetching UV index data: ${error.message}` },
+        { status: 500 }
+      );
+    }
+
     return NextResponse.json(
-      { error: `Error fetching UV index data: ${error.message}` },
+      { error: "An unknown error occurred while fetching UV index data" },
       { status: 500 }
     );
   }
